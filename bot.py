8229406f42eaa2bae5b922c9194280069d78a38d
Strike-boy <ogabekmahmudzonov24@gmail.com>
2025-07-23 11:58:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ API_TOKEN = '7936182138:AAHT25gYJuh2zU8-tk6yUVJOj9a5vmQeohk'
 ACR_HOST = "identify-ap-southeast-1.acrcloud.com"
 ACR_ACCESS_KEY = "e48f0d7b2af6ccad4015b26d57d75903"
 ACR_SECRET_KEY = "WTWOUirBwcIPMJY6vOHEXVKilaMviC8doHQKGgaV"
-VK_TOKEN = os.environ.get("VK_TOKEN", "vk1.a.28U7Vkhl8vYPj6dSs8R35XK6xvHHn-dBDAphdJuqb9GikBcmRaoc0XmcsUWFiUNfpumdyquLxARKUakREEr7QATvpasmSC5xvQYknN95dXjtZMHHD6WmIp24qTAed2AW9FIdsdT5cVEvtftGmSFsoMFoQ7kF8XQ6RYlGpbfHPczac3S9zru10VKajMcLPRRw6Fq0mx-rI9qYf6znolx0MQ")
+VK_TOKEN = "vk1.a.ZumqOfV5IBzYqoI_h08ZsjtAnxNrG2XZmG4cpEpn92Gv-8-hrx0Pe32UOwtGW1mo3t_f4P7iofdUiyjjMvsgxnzUWQjmu5pfxfUE4AK9_mQZKUknERO-PYiGirPYxhDI9R8NA0nnCoMu_HVk6tnCmSq8qevqHsoy0LK5NzgKEVq1_Xo2zgqv_CkYfz4-goL8_vo62DcTQ2uyO6RP6oHCgA"
 
 ADMIN_ID = 1001788720  # замени при необходимости
 broadcast_mode = False
